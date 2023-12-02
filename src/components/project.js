49015d2423806d7fbffcc3b19961af839b26ee43
Vladislav <vladislav.work98@gmail.com>
2023-12-02 19:08:32 +0200

    created project constructor

---
+++ src/components/project.js
@@ -0,0 +1,37 @@
+import ToDo from './toDo';
+import { nanoid } from 'nanoid';
+
+class Project {
+    #title;
+    #id = nanoid();
+    #toDos = [];
+
+    constructor(title) {
+        this.#title = title;
+    }
+
+    changeTitle = (newTitle) => {
+        this.#title = newTitle;
+    };
+
+    getTitle = () => {
+        return this.#title;
+    };
+
+    addToDo = (title, description, dueDate, isImportant, isDone) => {
+        this.#toDos = [
+            ...this.#toDos,
+            new ToDo(title, description, dueDate, isImportant, isDone),
+        ];
+    };
+
+    deleteToDo = (id) => {
+        this.#toDos = this.#toDos.filter((toDo) => !toDo.id === id);
+    };
+
+    getToDos = () => {
+        return this.#toDos;
+    };
+}
+
+export default Project;
